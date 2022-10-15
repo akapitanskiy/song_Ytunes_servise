7e0ed64be6510749b90b5bf8c6a4e7fc5d73fa7f
akapitanskiy <alexeykapitanskiy@gmail.com>
2022-10-15 10:35:02 +0400

    first commit

---
+++ app/src/main/java/uk/saltppay/android_developer_challenge_ntunfd/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.saltppay.android_developer_challenge_ntunfd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
